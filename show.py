b528e4a9a7f40cf222c386147416ace43bc14803
MatvienokVS <Skavaava@gmail.com>
2023-04-09 23:16:15 +0300

    Решение задач семинар 8

--- show.py
+++ show.py
@@ -2,18 +2,16 @@
 telefon_book = []
 
 
-def get_tb():
-    return telefon_book
+# def get_tb():
+#     return telefon_book
 
 
 def show_all():
     file = 'telefon_book.csv'
     with open(file, 'r', encoding='utf-8') as data:
-        data.readlines()
+        # data.readlines()
         for i in data:
-            i = i.strip().split(';')
-            telefon_book.append({'name': i[0],
-                                 'tel_number': i[1],
-                                 'function': i[2]})
+            print(f'{i}')
 
-# show_all()
+
+show_all()
